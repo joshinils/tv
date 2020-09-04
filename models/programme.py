1458a3e65ae3a2377076d6bdd7f89d3d1fbf99bd
Philipp Zettl <philipp@godesteem.de>
2020-09-04 17:56:48 +0200

    some examples

--- models/programme.py
+++ models/programme.py
@@ -9,12 +9,24 @@ class Programme:
         self.channel: Channel = channel
         self.start: datetime = start
         self.stop: datetime = stop
+        self.data: Dict = {}
+        self.is_prominent = False
 
-    @classmethod
-    def from_dict(cls, input_dict: Dict) -> 'Programme':
-        date_format = "%Y%m%d%H%M%S %z"
-        return cls(
+    @staticmethod
+    def from_dict(input_dict: Dict) -> 'Programme':
+        date_format: str = "%Y%m%d%H%M%S %z"
+        return Programme(
             ChannelManager.get_or_create(input_dict['channel']),
             datetime.strptime(input_dict['start'], date_format),
             datetime.strptime(input_dict['stop'], date_format)
         )
+
+    def __repr__(self):
+        return f'{self.channel.name}: {self.start} -> {self.stop}'
+
+    def __set__(self, instance, value):
+        if instance not in ['channel', 'start', 'stop']:
+            self.data[instance] = value
+
+    def __get__(self, instance, owner):
+        pass
