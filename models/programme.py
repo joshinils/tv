b98143929422e934e59680f680389410b54c505a
joshinils <joshua.laskowski@web.de>
2020-09-05 19:17:24 +0200

    add 2 hours everywhere, gonna be fun when summertime ends... fkn timezones man

--- models/programme.py
+++ models/programme.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Dict
 from .channelName import ChannelName
 
@@ -7,8 +7,8 @@ class Programme:
     def __init__(self, channelName: ChannelName, start: datetime, stop: datetime):
         self.channelName: ChannelName = channelName
         date_format: str = "%Y%m%d%H%M%S %z"
-        self.start = datetime.strptime(start, date_format)
-        self.stop = datetime.strptime(stop, date_format)
+        self.start = datetime.strptime(start, date_format) + timedelta(hours=2)
+        self.stop = datetime.strptime(stop, date_format) + timedelta(hours=2)
         self.data: Dict = {}
         self.prominent = False
         if self.start.hour >= 20 and self.start.minute >= 1:
